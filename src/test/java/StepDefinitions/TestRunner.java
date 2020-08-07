89a9aba48b5a065de745b4724bbc9a4c118e0ee1
wsieniawski <sieniawskidev@gmail.com>
2020-08-07 21:27:11 +0200

    added runner class with cucumber options

---
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -0,0 +1,17 @@
+package StepDefinitions;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features="src/test/resources/Features", glue= {"StepDefinitions"},
+		monochrome = true,
+		//plugin = {"pretty", "html:target/HtmlReports"}
+		//plugin = {"pretty", "json:target/JSONReports/report.json"}
+		plugin = {"pretty", "junit:target/JUnitReports/report.xml"}
+)
+public class TestRunner {
+
+}
